0f6ae42329422023f1d9370b69884d279ce3fdb9
Martin Dvorsky <dvorskymatko@gmail.com>
2026-03-26 21:19:50 +0100

    test autoupdate

--- lib/screens/database_screen.dart
+++ lib/screens/database_screen.dart
@@ -26,7 +26,7 @@ class _DatabaseScreenState extends State<DatabaseScreen> {
   final List<DatabaseSection> sections = [
     DatabaseSection(
       id: 'cities',
-      title: 'Mestá a obce a dano a mato a simon',
+      title: 'Mestá a obce a dano a mato a simon a miso',
       description: 'Správa miest a obcí v systéme',
       icon: Icons.location_city_rounded,
       color: Colors.blue,
